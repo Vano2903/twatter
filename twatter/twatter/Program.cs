09eaf1739cefbd30db455233d4e317c197c0e18f
Vanoncini Davide 18008 <vanoncini.18008@PALEOCAPA.DID>
2022-01-12 12:57:22 +0100

    ciao!

--- twatter/twatter/Program.cs
+++ twatter/twatter/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace twatter
 {
@@ -20,14 +24,17 @@ namespace twatter
         public int PAYLOAD_TOO_LARGE = 413;
         public int INTERNAL_SERVER_ERROR = 500;
         public int NOT_IMPLEMENTED = 501;
+        public bool SERVER_LISTENING = true;
 
         private Dictionary<string, string> pages;
         private string ip;
         private int port;
+        private HttpListener listener;
 
         public webServer( string ip, int port)
         {
-            this.pages = new Dictionary<string, string>();
+            listener = new HttpListener();
+            pages = new Dictionary<string, string>();
             this.ip = ip;
             this.port = port;
         }
@@ -39,9 +46,61 @@ namespace twatter
             int i = 0;
             foreach (string page in pages)
             {
-                string readText = File.ReadAllText("./pages/"+page);
+                string readText = File.ReadAllText(@"\pages\"+page);
                 this.pages.Add(indexes[i], readText);
                 i++;
+            }
+        }
+
+        public void listenAndServe()
+        {
+            listener.Prefixes.Add(ip + port);
+            listener.Start();
+            try
+            {
+                Task listenTask = connectionHandler();
+                listenTask.GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void pause()
+        {
+            SERVER_LISTENING = false;
+        }
+
+        public void resume()
+        {
+            SERVER_LISTENING = true;
+        }
+
+        public void kill()
+        {
+            listener.Close();
+        }
+
+        public async Task connectionHandler()
+        {
+            while (SERVER_LISTENING)
+            {
+                HttpListenerContext ctx = await listener.GetContextAsync();
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+
+            }
+            if (!SERVER_LISTENING)
+            {
+                HttpListenerContext ctx = await listener.GetContextAsync();
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+
+                byte[] data = Encoding.UTF8.GetBytes("{\"message\": \"Credenziali scorrette, utente non riconosciuto\", \"accepted\": false}");
+                resp.ContentType = "application/json";
+                await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                resp.Close();
             }
         }
     }
